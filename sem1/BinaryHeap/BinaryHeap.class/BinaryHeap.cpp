e22a9372095a5c6f656535ed895595b2fc972180
Tema Smirnov <git.tema@smirnov.one>
2020-01-13 16:50:57 +0300

    [C++] Добавлена реализация двоичной/бинарной кучи
    https://neerc.ifmo.ru/wiki/index.php?title=%D0%94%D0%B2%D0%BE%D0%B8%D1%87%D0%BD%D0%B0%D1%8F_%D0%BA%D1%83%D1%87%D0%B0

---
+++ sem1/BinaryHeap/BinaryHeap.class/BinaryHeap.cpp
@@ -0,0 +1,162 @@
+﻿#pragma once
+
+/*****************************************************************************
+ *
+ *  AUTHOR:      Artem Smirnov
+ *  DATE:        2020-01-13
+ *  FILE:        BinaryHeap.cpp
+ *  PURPOSE:     C++ implementation of Binary Heap
+ *
+ *****************************************************************************/
+
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <iterator>
+#include <io.h>
+#include <fcntl.h>
+
+using namespace std;
+
+/// <summary>Двоиная куча</summary>
+class BinaryHeap
+{
+private:
+    /// <summary>Вектор, содержащий данные кучи</summary>
+    vector<int> heap;
+
+    /// <summary>Левый потомок</summary>
+    /// <param name="parent">Узел-родитель</summary>
+    int left(int parent)
+    {
+        int l = 2 * parent + 1;
+        return l < heap.size() ? 1 : -1;
+    }
+
+    /// <summary>Правый потомок</summary>
+    /// <param name="parent">Узел-родитель</summary>
+    int right(int parent)
+    {
+        int r = 2 * parent + 2;
+        return r < heap.size() ? r : -1;
+    }
+
+    /// <summary>Родительский узел</summary>
+    /// <param name="child">Узел-потомок</summary>
+    int parent(int child)
+    {
+        int p = (child - 1) / 2;
+        return child == 0 ? -1 : p;
+    }
+
+    /// <summary>Упорядочение кучи: снизу-вверх</summary>
+    /// <remarks>
+    /// Временная сложность операции: O(log2 N).
+    /// Восстанавливает основное свойство кучи для дерева с корнем в i-ой вершине при условии, что оба поддерева ему удовлетворяют.
+    /// Алгоритм "поднимает" i-ую вершину (меняет местами с родителем), пока не будет соблюдено основное свойство кучи.
+    /// (процесс завершится, когда не найдется родителя, меньшего своего потомка)
+    /// </remarks>
+    /// <param name="index">Индекс узла</summary>
+    void heapifyUp(int index)
+    {
+        if (index >= 0 && parent(index) >= 0 && heap[parent(index)] > heap[index])
+        {
+            int temp = heap[index];
+            heap[index] = heap[parent(index)];
+            heap[parent(index)] = temp;
+            heapifyUp(parent(index));
+        }
+    }
+
+    /// <summary>Упорядочение кучи: сверху-вниз</summary>
+    /// <remarks>
+    /// Временная сложность операции: O(log2 N).
+    /// Восстанавливает основное свойство кучи для дерева с корнем в i-ой вершине при условии, что оба поддерева ему удовлетворяют.
+    /// Алгоритм "опускает" i-ую вершину (меняет местами с наименьшим из потомков), пока не будет соблюдено основное свойство кучи.
+    /// (процесс завершится, когда не найдется потомка, большего своего потомка)
+    /// </remarks>
+    /// <param name="index">Индекс узла</summary>
+    void heapifyDown(int index)
+    {
+        int child = left(index);
+        int child1 = right(index);
+        if (child >= 0 && child1 >= 0 && heap[child] > heap[child1])
+        {
+            child = child1;
+        }
+        if (child > 0 && heap[index] > heap[child])
+        {
+            int temp = heap[index];
+            heap[index] = heap[child];
+            heap[child] = temp;
+            heapifyDown(child);
+        }
+    }
+
+public:
+    /// <summary>Конструктор кучи</summary>
+    BinaryHeap(){};
+    /// <summary>Деструкор кучи</summary>
+    ~BinaryHeap(){};
+
+    /// <summary>Размер кучи</summary>
+    /// <returns>Возвращает размер кучи в Int32</returns>
+    int Size()
+    {
+        return heap.size();
+    }
+
+    /// <summary>Операция добавления элемента в кучу</summary>
+    /// <remarks>
+    /// Временная сложность операции: O(log2 N).
+    /// Новый элемент добавляется на последнее место в куче/массиве, то есть позицию с максимальным индексом.
+    /// Возможно, что при этом будет нарушено основное свойство кучи, т.к. новый элемент может быть больше родителя.
+    /// В таком случае новый элемент "поднимается" на один уровень (меняется с вершиной-родителем) до тех пор, пока не будет соблюдено основное свойство кучи.
+    /// </remarks>
+    void Insert(int element)
+    {
+        heap.push_back(element);
+        heapifyUp(heap.size() - 1);
+    }
+
+    /// <summary>Операция удаления минимального элемента из кучи</summary>
+    /// <remarks>
+    /// В упорядоченной куче "сверху-вниз" максимальный элемент всегда хранится в корне.
+    /// Алгоритм восстанавлиает упорядоченность двоичной кучи после удаления максимального элемента, устанавливая на его место последний элемент и производя операцию упорядочения кучи "сверху-вниз".
+    /// </remarks>
+    /// <returns>Возвращает true - операция выполнена, false - куча не имеет узлов (пустая)</returns>
+    bool DeleteMin()
+    {
+        if (heap.size() == 0)
+        {
+            // wcout << "Куча пуста" << endl;
+            return false;
+        }
+        heap[0] = heap.at(heap.size() - 1);
+        heap.pop_back();
+        heapifyDown(0);
+        // wcout << "Элемент удалён" << endl;
+        return true;
+    }
+
+    /// <summary>Операция получения минимального элемента из кучи</summary>
+    /// <remarks>Временная сложность операции: O(1)</remarks>
+    /// <returns>Возвращает минимальный размер кучи в Int32</returns>
+    int GetMin()
+    {
+        return heap.size() == 0 ? -1 : heap.front();
+    }
+
+    /// <summary>Операция вывода кучи в stdout</summary>
+    void DisplayHeap()
+    {
+        vector<int>::iterator pos = heap.begin();
+        // wcout << "Куча -->  ";
+        while (pos != heap.end())
+        {
+            wcout << *pos << " ";
+            pos++;
+        }
+        wcout << endl;
+    }
+};
---
+++ sem1/BinaryHeap/main.cpp
@@ -0,0 +1,73 @@
+﻿#pragma once
+#include <iostream>
+#include <io.h>
+#include <fcntl.h>
+
+using namespace std;
+
+#include "BinaryHeap.class/BinaryHeap.cpp"
+
+int wmain(int argc, wchar_t* argv[])
+{
+    // setlocale(LC_ALL, "Russian");
+
+    _setmode(_fileno(stdout), _O_U16TEXT);
+    _setmode(_fileno(stdin), _O_U16TEXT);
+    _setmode(_fileno(stderr), _O_U16TEXT);
+
+    BinaryHeap binaryHeap;
+    while (1)
+    {
+        wcout << "\n------------------" << endl;
+        wcout << L"Операции с кучей" << endl;
+        wcout << "------------------" << endl;
+        wcout << L"1) Добавить элемент" << endl;
+        wcout << L"2) Удалить минимальный элемент" << endl;
+        wcout << L"3) Получить минимальный элемент" << endl;
+        wcout << L"4) Вывести кучу" << endl;
+        wcout << L"5) Выход" << endl;
+        int choice, element;
+        wcout << L"Выберите операцию:\n" << endl;
+
+        wcin >> choice;
+        switch (choice)
+        {
+        case 1:
+            wcout << L"Введите число, которое должно быть помещено в кучу: ";
+            wcin >> element;
+            binaryHeap.Insert(element);
+            break;
+        case 2:
+            wcout << L"Удаление минимального элемента: ";
+            if (binaryHeap.GetMin() == -1)
+            {
+                wcout << L"Куча пуста" << endl;
+            }
+            else
+            {
+                int el = binaryHeap.GetMin();
+                binaryHeap.DeleteMin();
+                wcout << L"Элемент удалён: " << el << endl;
+            }
+            break;
+        case 3:
+            wcout << L"Минимальный элемент: ";
+            if (binaryHeap.GetMin() == -1)
+            {
+                wcout << L"Куча пуста" << endl;
+            }
+            else
+                wcout << L"Минимальный элемент:  " << binaryHeap.GetMin() << endl;
+            break;
+        case 4:
+            wcout << L"Элементы кучи:  ";
+            binaryHeap.DisplayHeap();
+            break;
+        case 5:
+            exit(1);
+        default:
+            wcout << L"\nВыберите пункт из меню, указав его номер" << endl;
+        }
+    }
+    return 0;
+}
